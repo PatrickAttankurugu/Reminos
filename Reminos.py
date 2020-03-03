2dda0734929645a445686569fe09ff20e5469260
Patrick Attankurugu <patricka.azuma@gmail.com>
2020-03-03 07:10:02 +0000

    I added a Reminos.py file

---
+++ Reminos.py
@@ -0,0 +1,40 @@
+import argparse
+
+import MySQLdb
+import requests,urllib
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from datetime import timedelta
+import datetime
+
+import os
+import sys
+
+
+
+#This will send an SMS to your configured admin phone. I think we will use Twilio 
+#has an option for configuring a client's phone in order for them to receive the birthday wish
+#Well let's see how it goes...
+
+class SendSMS:
+
+	def sendToAdmin():
+  	  	
+    
+	def sendToClient():
+
+
+
+class SendEmail:
+	def SendToAdmin():
+		pass
+
+
+
+	def SendToClient():
+		pass
+
+
